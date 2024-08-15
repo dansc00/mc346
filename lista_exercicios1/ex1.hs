829e026e60c38b90a84d2f977b353d4bb44f8eed
dansc00 <danielsousa.cipriano@gmail.com>
2024-08-15 18:52:52 -0300

    Adição do diretório para a lista de exercicios 1. Adição do exercicio 1.

---
+++ lista_exercicios1/ex1.hs
@@ -0,0 +1,9 @@
+-- faça um programa que retorne o tamanho de uma lista
+
+len [] = 0 
+len (x:xs) = 1 + len xs
+
+main = do
+    let list = [1,2,3,4,5]
+    let lenList = len list
+    print lenList
