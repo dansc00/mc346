34f8e6e1ce74a244656f12b81a576faee3ae6ae8
Daniel de Sousa Cipriano <ra233228@xaveco.lab.ic.unicamp.br>
2024-08-17 22:33:06 -0300

    Correção do exercicio 7

--- haskell/lista_exercicios1/ex7/ex7.hs
+++ haskell/lista_exercicios1/ex7/ex7.hs
@@ -1,1 +1,8 @@
 -- Recebe uma posição n e imprime a lista invertida de n até a posição 1
+
+rangeRev _ _ [] = []
+rangeRev count n [x:xs] = rangeRev count n xs 
+    count = count + 1
+    if count == n
+    then print x
+    else 
