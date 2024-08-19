9ba445ff3492e211a5a59a490442ae79fbde7e51
Daniel de Sousa Cipriano <ra233228@xaveco.lab.ic.unicamp.br>
2024-08-18 21:24:02 -0300

    Adição do exercicio 7

--- haskell/lista_exercicios1/ex7/ex7.hs
+++ haskell/lista_exercicios1/ex7/ex7.hs
@@ -1,8 +1,17 @@
--- Recebe uma posição n e imprime a lista invertida de n até a posição 1
+-- Recebe uma posição n e imprime a lista invertida de n até a primeira posição
 
-rangeRev _ _ [] = []
-rangeRev count n [x:xs] = rangeRev count n xs 
-    count = count + 1
-    if count == n
-    then print x
-    else 
+getElement [] _ = error "Indice inválido"
+getElement (x:xs) 0 = x
+getElement (x:xs) n = getElement xs (n-1)
+
+addElem x xs = x : xs
+
+rangeRev [] _ = []
+rangeRev [x] _ = [x]
+rangeRev xs 0 = [getElement xs 0]
+rangeRev xs n = getElement xs n : rangeRev xs (n-1)
+
+main = do
+    let list = [1,2,3,4,5,6,7,8,9,10]
+    let revList = rangeRev list 3
+    print revList
