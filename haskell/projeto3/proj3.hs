a6629deb8fb7a99e9dc77e2f4595c7816fdd1573
Daniel de Sousa Cipriano <ra233228@xaveco.lab.ic.unicamp.br>
2024-09-18 21:44:28 -0300

    Finalização do projeto 3

--- haskell/projeto3/proj3.hs
+++ haskell/projeto3/proj3.hs
@@ -1,19 +1,32 @@
 
--- recebe chave e contador em formato de lista de tuplas, incrementa tupla que possui a chave passada
-incrementar chave contador = map (\x -> if (fst x == chave) then (chave, (snd x)+1) else (fst x, snd x)) contador
+import qualified Data.Map.Strict as Map
+import qualified Data.Char as DC 
 
-agruparChaves chave contador
-    | (valChave == valX) || (valChave == valX+32) = (chave,contador) : incrementar chave xs 
-    | otherwise = incrementar chave xs
-    where
-        valChave = fromEnum chave -- valor inteiro do caracter pela tabela ASCII
-        valX = fromEnum x
+-- recebe chave e contador em formato de lista de tuplas, incrementa contador da tupla que possui a chave passada
+incrementar chave contador = Map.insertWith (+) chave 1 contador
 
--- constrói contador em formato de lista de tuplas, com uma ocorrência de cada chave
-construirContador [] = []
-construirContador (x:xs) = (x,1) : construirContador xs
- 
-letraMaisComum string = 
+-- constrói contador em formato de lista de tuplas
+construirContador [] contador = contador
+construirContador (x:xs) contador = construirContador xs contadorAtualizado
     where
-        listaFiltrada = filter (\ x -> (x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z')) string -- string com apenas letras
-        contador = construirContador listaFiltrada 
+        contadorAtualizado = incrementar x contador
+
+-- retorna chave associada ao maior valor do contador
+filtraMaximo [] max chave = chave
+filtraMaximo (x:xs) max chave
+    | (snd x) > max = filtraMaximo xs (snd x) (fst x)
+    | otherwise = filtraMaximo xs max chave 
+
+-- retorna letra com mais ocorrências
+letraMaisComum string = res
+    where
+        stringFiltrada = map (\x -> DC.toLower x) (filter (\ x -> (x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z')) string) -- string com apenas letras minusculas
+        contadorInicial = Map.empty
+        contadorFinal = Map.toList (construirContador stringFiltrada contadorInicial)
+        res = filtraMaximo contadorFinal 0 ' ' 
+
+main = 
+    do
+    let string = "77,88 a!? abc BB 8 8    8  fyt"
+    let res = letraMaisComum string
+    print res
